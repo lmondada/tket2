ff0baf4e02cd66629f2ba0ab32e962acea92a6be
Luca Mondada <luca@mondada.net>
2025-08-06 15:38:18 +0200

    clean up tests

--- tket/src/rewrite/tuple_impls.rs
+++ tket/src/rewrite/tuple_impls.rs
@@ -1,3 +1,5 @@
+#![allow(non_snake_case)]
+
 //! Tuple implementations for the [`Rewriter`] trait.
 //!
 //! This module provides implementations of [`Rewriter`] for tuples of rewriters,
@@ -8,8 +10,6 @@ use crate::Circuit;
 use hugr::HugrView;
 
 /// Macro to generate Rewriter implementations for tuples of various sizes.
-///
-/// This generates implementations for tuples containing 2-8 rewriters.
 macro_rules! impl_rewriter_for_tuples {
     // Generate implementation for a specific tuple size
     ($(($($generic:ident),+)),+) => {
@@ -32,7 +32,6 @@ macro_rules! impl_rewriter_for_tuples {
 }
 
 // Generate implementations for tuples of size 2 through 8
-#[allow(non_snake_case)]
 impl_rewriter_for_tuples! {
     (R1, R2),
     (R1, R2, R3),
@@ -46,20 +45,31 @@ impl_rewriter_for_tuples! {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::TketOp;
     use hugr::{
         builder::{endo_sig, DFGBuilder, Dataflow, DataflowHugr},
         extension::prelude::qb_t,
+        hugr::views::SiblingSubgraph,
+        ops::handle::DfgID,
     };
-    use crate::TketOp;
 
     /// Mock rewriter for testing that returns a specific number of empty rewrites
     #[derive(Clone, Debug)]
     struct MockRewriter;
 
     impl Rewriter<hugr::Node> for MockRewriter {
-        fn get_rewrites(&self, _circ: &Circuit<impl HugrView<Node = hugr::Node>>) -> Vec<CircuitRewrite<hugr::Node>> {
-            // Return empty vec for testing - the count `self.0` is used for verification in tests
-            vec![]
+        fn get_rewrites(
+            &self,
+            circ: &Circuit<impl HugrView<Node = hugr::Node>>,
+        ) -> Vec<CircuitRewrite<hugr::Node>> {
+            // Return a single (dummy) rewrite
+            vec![CircuitRewrite::try_new(
+                &SiblingSubgraph::try_new_dataflow_subgraph::<_, DfgID>(circ.to_owned().hugr())
+                    .unwrap(),
+                circ.hugr(),
+                circ.to_owned(),
+            )
+            .unwrap()]
         }
     }
 
@@ -72,81 +82,22 @@ mod tests {
         Circuit::new(h.finish_hugr_with_outputs(qbs).unwrap())
     }
 
-    #[test]
-    fn test_tuple_2() {
-        let circuit = create_test_circuit();
-        let rewriter = (MockRewriter, MockRewriter);
-        let rewrites = rewriter.get_rewrites(&circuit);
-        assert_eq!(rewrites.len(), 0); // Both mock rewriters return empty vecs
-    }
+    use rstest::rstest;
 
-    #[test]
-    fn test_tuple_3() {
+    #[rstest]
+    #[case((MockRewriter, MockRewriter), 2)]
+    #[case((MockRewriter, MockRewriter, MockRewriter), 3)]
+    #[case((MockRewriter, MockRewriter, MockRewriter, MockRewriter), 4)]
+    #[case((MockRewriter, MockRewriter, MockRewriter, MockRewriter, MockRewriter), 5)]
+    #[case((MockRewriter, MockRewriter, MockRewriter, MockRewriter, MockRewriter, MockRewriter), 6)]
+    #[case((MockRewriter, MockRewriter, MockRewriter, MockRewriter, MockRewriter, MockRewriter, MockRewriter), 7)]
+    #[case((MockRewriter, MockRewriter, MockRewriter, MockRewriter, MockRewriter, MockRewriter, MockRewriter, MockRewriter), 8)]
+    fn test_tuple_rewriters_empty_vecs<R>(#[case] rewriter: R, #[case] expected_len: usize)
+    where
+        R: Rewriter<hugr::Node>,
+    {
         let circuit = create_test_circuit();
-        let rewriter = (MockRewriter, MockRewriter, MockRewriter);
         let rewrites = rewriter.get_rewrites(&circuit);
-        assert_eq!(rewrites.len(), 0); // All mock rewriters return empty vecs
-    }
-
-    #[test]
-    fn test_tuple_4() {
-        let circuit = create_test_circuit();
-        let rewriter = (MockRewriter, MockRewriter, MockRewriter, MockRewriter);
-        let rewrites = rewriter.get_rewrites(&circuit);
-        assert_eq!(rewrites.len(), 0); // All mock rewriters return empty vecs
-    }
-
-    #[test]
-    fn test_tuple_5() {
-        let circuit = create_test_circuit();
-        let rewriter = (
-            MockRewriter, MockRewriter, MockRewriter, 
-            MockRewriter, MockRewriter
-        );
-        let rewrites = rewriter.get_rewrites(&circuit);
-        assert_eq!(rewrites.len(), 0); // All mock rewriters return empty vecs
-    }
-
-    #[test]
-    fn test_tuple_6() {
-        let circuit = create_test_circuit();
-        let rewriter = (
-            MockRewriter, MockRewriter, MockRewriter, 
-            MockRewriter, MockRewriter, MockRewriter
-        );
-        let rewrites = rewriter.get_rewrites(&circuit);
-        assert_eq!(rewrites.len(), 0); // All mock rewriters return empty vecs
-    }
-
-    #[test]
-    fn test_tuple_7() {
-        let circuit = create_test_circuit();
-        let rewriter = (
-            MockRewriter, MockRewriter, MockRewriter, MockRewriter,
-            MockRewriter, MockRewriter, MockRewriter
-        );
-        let rewrites = rewriter.get_rewrites(&circuit);
-        assert_eq!(rewrites.len(), 0); // All mock rewriters return empty vecs
-    }
-
-    #[test]
-    fn test_tuple_8() {
-        let circuit = create_test_circuit();
-        let rewriter = (
-            MockRewriter, MockRewriter, MockRewriter, MockRewriter,
-            MockRewriter, MockRewriter, MockRewriter, MockRewriter,
-        );
-        let rewrites = rewriter.get_rewrites(&circuit);
-        assert_eq!(rewrites.len(), 0); // All mock rewriters return empty vecs
-    }
-
-    #[test]
-    fn test_mixed_tuple_sizes() {
-        let circuit = create_test_circuit();
-        
-        // Test that different rewriter combinations work correctly
-        let rewriter_mixed = (MockRewriter, MockRewriter, MockRewriter);
-        let rewrites = rewriter_mixed.get_rewrites(&circuit);
-        assert_eq!(rewrites.len(), 0); // All mock rewriters return empty vecs
+        assert_eq!(rewrites.len(), expected_len);
     }
 }
